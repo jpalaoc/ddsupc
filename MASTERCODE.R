cf2d09716712051ac63b8f93f3388a88ce147256
JuanP <jpalao1@gmail.com>
2022-05-06 17:50:46 -0500

    Graphs

--- MASTERCODE.R
+++ MASTERCODE.R
@@ -10,5 +10,5 @@ p <- ggplot(iris, aes(x = Sepal.Width, y = Sepal.Length, colour=Species))
 p <- p + geom_point(aes(shape = Species))
 p <- p + xlab("sepal Length") + ylab("sepal Width") + ggtitle("SEPAL LENGTH AND WIDTH")
 p <- p + geom_smooth(method = "lm")
-p + facet_grid(. ~ Species)
+p + facet_grid(Species ~ .)
 
